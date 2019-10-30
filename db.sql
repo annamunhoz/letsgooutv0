278bdd2537c08c09671140a1a9e6f8eb8d511013
Anna Munhoz <anninha.omg@gmail.com>
2019-10-30 18:31:20 -0300

    update sql create table

--- db.sql
+++ db.sql
@@ -1,12 +1,7 @@
--- Table: public."PLACES"
-
--- DROP TABLE public."PLACES";
-
 CREATE TABLE public."PLACES"
 (
-    "ID" integer NOT NULL DEFAULT nextval('"PLACES_ID_seq"'::regclass),
-    name character varying(250) COLLATE pg_catalog."default" NOT NULL,
-    price character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    category character varying(500) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "PLACES_pkey" PRIMARY KEY ("ID")
+    "ID" serial PRIMARY KEY,
+    name character varying(250) NOT NULL,
+    price character varying(100) NOT NULL,
+    category character varying(500) NOT NULL
 )
